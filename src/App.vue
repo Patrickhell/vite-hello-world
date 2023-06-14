2ed9812039e2ab4eb1b347df5b5fe12ac4faf238
Patrickhell <patrickhell19@yahoo.fr>
2023-06-14 20:38:46 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+import AppTitle from './components/AppTitle.vue';
+
+export default (await import('vue')) .defineComponent({
+  name : 'mainApp',
+  components:{
+    AppTitle
+  }
+})
+</script>
+
+<template>
+  <AppTitle />
+</template>
+
+<style scoped>
+
+</style>
